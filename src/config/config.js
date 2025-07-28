39b7d9a3e4eba8d5be500edc059c36040503b53b
Hilmy Zhafran Day Taqy <hilmyzdt@gmail.com>
2025-07-28 20:59:54 +0700

    refactor: update database configuration to use DATABASE_URL and improve SSL settings

--- src/config/config.js
+++ src/config/config.js
@@ -2,12 +2,14 @@ require('dotenv').config();
 
 module.exports = {
   development: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   },
   test: {
     username: process.env.DB_USER || "root",
@@ -17,11 +19,13 @@ module.exports = {
     dialect: process.env.DB_DIALECT || "mysql"
   },
   production: {
-    username: process.env.DB_USER,
-    password: process.env.DB_PASS,
-    database: process.env.DB_NAME,
-    host: process.env.DB_HOST,
-    port: process.env.DB_PORT,
-    dialect: process.env.DB_DIALECT
+    use_env_variable: 'DATABASE_URL',
+    dialect: 'postgres',
+    dialectOptions: {
+      ssl: {
+        require: true,
+        rejectUnauthorized: false
+      }
+    }
   }
 };
